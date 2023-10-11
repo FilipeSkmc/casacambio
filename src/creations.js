d02ddde50a89bd5567bd68c0df2797a7c7508cfb
FilipeSkmc <filipe-santana@outlook.com>
2023-10-11 11:34:30 -0300

    fix: functions

--- src/creations.js
+++ src/creations.js
@@ -1,12 +1,12 @@
 const listaMoedas = document.querySelector('.lista-moedas');
 const titleMoedas = document.querySelector('.moedas-title');
 
-const BASE_CONVERTION = 1000;
+const BASE_CONVERSION = 1000;
 
 const createElementLi = (coin) => {
   const liElement = document.createElement('li'); // cria um elemento li
 
-  const date = new Date(coin.timestamp * BASE_CONVERTION); // converte para data
+  const date = new Date(coin.timestamp * BASE_CONVERSION); // converte para data
   const localDate = (date.toLocaleDateString('pt-BR')); // formata de acordo com o requisito
 
   // adiciona a classe coin
